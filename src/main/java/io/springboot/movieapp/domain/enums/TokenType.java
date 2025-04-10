615a06161e5fdb6406b98d1e278d2a367b5f7b85
phoebe-george <84939421+phoebe-george@users.noreply.github.com>
2025-04-10 23:05:32 +0200

    delet src

--- src/main/java/io/springboot/movieapp/domain/enums/TokenType.java
+++
@@ -1,6 +0,0 @@
-package io.springboot.movieapp.domain.enums;
-
-public enum TokenType {
-    BEARER
-}
-
--- src/main/java/io/springboot/movieapp/domain/enums/UserRole.java
+++
@@ -1,5 +0,0 @@
-package io.springboot.movieapp.domain.enums;
-
-public enum UserRole {
-    ADMIN, USER
-}
